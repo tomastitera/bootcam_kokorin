227c9d6b3d4e344ef99b662d1795768691b668e3
Unknown <tomas.titera@gmail.com>
2018-06-27 18:37:25 +0200

    Rothko voe
    čtverec umčo neasi

---
+++ rothko/empty-example/sketch.js
@@ -0,0 +1,15 @@
+// do not remove, allowes autocomplete
+/// <reference path="./../p5.global-mode.d.ts" />
+
+function setup() {
+  
+  createCanvas(400, 400);
+  
+  fill("red");
+
+  rect(10, 10, 100, 100);
+}
+
+function draw() {
+  // put drawing code here
+}
