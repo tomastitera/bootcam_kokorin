605a16030bf5353723253753f82225cefd60549b
Unknown <tomas.titera@gmail.com>
2018-06-29 11:46:43 +0200

    rothko
    2

--- rothko/empty-example/sketch.js
+++ rothko/empty-example/sketch.js
@@ -1,19 +1,47 @@
 // do not remove, allowes autocomplete
 /// <reference path="./../p5.global-mode.d.ts" />
 
+var clicks = 10;
+var keys = 10;
+var time = 0;
+var timespeed = 1;
+var myHugh = 0;
+
 function setup() {
-  
   createCanvas(400, 400);
-  
-  fill("red");
-
-  rect(10, 10, 100, 100);
-  
-  fill("pink");
-
-  triangle(150, 450, 300, 300, 450, 150);
+  angleMode(DEGREES);
+  colorMode(HSB,100);
 }
 
-function draw() {
-  // put drawing code here
+function draw() { 
+  time = time + timespeed;  
+  var ellipseOffset = time % 400;
+  var myHugh = time % 100;
+
+  if (ellipseOffset > 300) {
+    background(myHugh,100,100);
+  } else {
+    background(100,100,100);
+  }
+
+  if (keyIsPressed) {
+    timespeed = 50;
+  } else {
+    timespeed = 1;
+  }
+  
+
+  ///stroke("pink");
+  fill("black");
+
+  ellipse(ellipseOffset, 200, clicks * 10, clicks * 10);
+  ellipse(300, 300, clicks * 10, clicks * 10);
 }
+
+function mouseReleased() {
+  clicks = clicks + 1;
+}
+
+function keyReleased() {
+  keys = keys + 1;
+} 
